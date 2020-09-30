630b9bb0eccc44ea9b456cafcde729bb0eb7dfa3
LindseySongstad <lindsey.songstad@outlook.com>
2020-09-30 09:28:37 -0700

    Assignment Step 134

---
+++ Basic_HTML_and_JavaScript1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Hello, World");
+
+function MyFirstFunction() {
+	var String = "Kiss me, I'm Irish!";
+	var result = String.fontcolor("green");
+	document.getElementById("Irish").innerHTML = result;
+}
